8f9819dc9e45fa1c8df3d73d6a67ee19c280aaab
Derek Chan <derekchan916@gmail.com>
2015-12-18 00:15:07 -0500

    Expand recorder components.

--- frontend/components/recorder.jsx
+++ frontend/components/recorder.jsx
@@ -5,6 +5,49 @@ var React = require('react'),
 var Recorder = React.createClass({
   getInitialState: function() {
     return ({ recording: false, track: new Track()})
+  },
+
+  render: function() {
+    return (
+      <div classNmae="controls">
+        <h3>Recorder</h3>
+        <button onClick={this.recordClick} className="record-button">
+          { this.recordingMessage() }
+        </button>
+        { this.trackSavingElements() }
+        <button onClick={this.playClick} className={this.playClass()}>
+          Play
+        </button>
+      </div>
+    )
+  },
+
+  recordingMessage: function () {
+    if (this.isRecording()) {
+      return "Stop Recording";
+    } else if (this.isDoneRecording()) {
+      return "Done Recording";
+    } else {
+      return "Start Recording";
+    }
+  },
+
+  playClick: function (e) {
+    if(!this.isTrackNew()){
+      this.state.track.play();
+    }
+  },
+
+  isTrackNew: function () {
+    return this.state.track.isBlank();
+  },
+
+  playClass: function () {
+    return "play-button" + this.isTrackNew() ? "" : " disabled";
+  },
+
+  isDoneRecording: function () {
+    return !this.isTrackNew() && !this.state.recording;
   }
 });
 
