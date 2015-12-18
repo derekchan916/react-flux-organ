72a33c881288081c018dcde7b272a7d1ca907ef9
Derek Chan <derekchan916@gmail.com>
2015-12-17 21:17:41 -0500

    Add Track util file, with startRecording and addNotes function.

---
+++ frontend/util/track.js
@@ -0,0 +1,13 @@
+var track = function (name){
+  this.name = name;
+  this.roll = [];
+};
+
+var startRecording = function() {
+  this.roll = [];
+  this.time = new Date();
+};
+
+var addNotes = function(note) {
+
+};
